be9478efc82f83f0c070cde9b145d7c76594d89a
Anton Kovalenko <anton@sw4me.com>
2010-11-27 03:00:17 +0300

    * src/runtime/gencgc.c: Typo/thinko: swapped os_validate_commit and os_validate
    * version.lisp-expr: bump

--- src/runtime/gencgc.c
+++ src/runtime/gencgc.c
@@ -537,9 +537,9 @@ void zero_pages_with_mmap(page_index_t start, page_index_t end) {
 
     os_invalidate(addr, length);
 #if defined(LISP_FEATURE_WIN32)
-    new_addr = os_validate(addr, length);
-#else
     new_addr = os_validate_recommit(addr, length);
+#else
+    new_addr = os_validate(addr, length);
 #endif
     if (new_addr == NULL || new_addr != addr) {
         lose("remap_free_pages: page moved, 0x%08x ==> 0x%08x",
