a0079d6636f6fa11a3f0e4fbcf5a8727a9ecbda6
Anton Kovalenko <anton@sw4me.com>
2011-03-12 04:20:44 +0300

    * src/runtime/win32-os.c (carry_frame_pointer): canonicalized whitespace

--- src/runtime/win32-os.c
+++ src/runtime/win32-os.c
@@ -2839,7 +2839,7 @@ os_context_register_t carry_frame_pointer(os_context_register_t default_value)
 {
     struct thread* self = arch_os_get_current_thread();
     return
-	(self->gc_safepoint_context) ?
+        (self->gc_safepoint_context) ?
         *os_context_fp_addr(self->gc_safepoint_context) : default_value;
 }
 
