3b8bfbedda67c1c79981592841fe9f5e1ca6ec5c
Anton Kovalenko <anton@sw4me.com>
2011-03-29 18:24:44 +0400

    * branch-version.lisp-expr: bump
    * src/code/win32.lisp (unixlike-open): also seek to the beginning if
      append is not specified.

--- src/code/win32.lisp
+++ src/code/win32.lisp
@@ -1018,10 +1018,9 @@ UNIX epoch: January 1st 1970."
                       (otherwise (- error-code)))))
           (progn
             (initialize-comm-timeouts handle)
-            (when (plusp (logand sb!unix::o_append flags))
-              ;; FIXME: seeking to the end is not enough for real APPEND
-              ;; semantics, but it's better than nothing.
-              (set-file-pointer-ex handle 0 2))
+            ;; FIXME: seeking to the end is not enough for real APPEND
+            ;; semantics, but it's better than nothing.
+            (set-file-pointer-ex handle 0 (if (plusp (logand sb!unix::o_append flags)) 2 0))
             (let ((fd (open-osfhandle handle (logior sb!unix::o_binary flags))))
               (if (minusp fd)
                   (values nil (sb!unix::get-errno))
